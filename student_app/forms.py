007b78ba603d80b0cb542314aa568202115b959d
Shivansh mehta <112550037+ShivanshMehtaa@users.noreply.github.com>
2022-09-24 18:10:23 +0530

    Add files via upload

---
+++ student_app/forms.py
@@ -0,0 +1,24 @@
+from socket import fromshare
+from django import forms
+from.models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model=Student
+        fields=['student_number', 'first_name', 'last_name','email', 'subject', 'gpa']
+        labels={
+            'student_number':'Student Number',
+            'first_name': 'First Name',
+            'last_name':'Last Name',
+            'email':'Email',
+            'subject':'Subject',
+            'gpa':'GPA',
+        }
+        widgets={
+            'student_number':forms.NumberInput(attrs={'class':'form-control'}),
+            'first_name':forms.TextInput(attrs={'class':'form-control'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'subject':forms.TextInput(attrs={'class':'form-control'}),
+            'gpa':forms.NumberInput(attrs={'class':'form-control'}),
+        }
